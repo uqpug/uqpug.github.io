8e5523a9b952125d974bc18b6744e577b525adfc
camwest5 <118239146+camwest5@users.noreply.github.com>
2026-04-24 14:34:49 +1000

    Follow up Apr, prepare May (functions)
    ** May PUG prep **
    
    * `ThePythonTutorial/3-Functions.ipynb` written up
    * Notebook prepared
    
    **April PUG follow up**
    
    * Questions and attendence updated
    * ThePythonTutorial/2-... updated with live changes
    * `Demonstrations/example_data_analysis_project` demonstrates typical project directory structure

---
+++ Demonstrations/example_data_analysis_project/analysis_3.py
@@ -0,0 +1,3 @@
+import common_functions as cf
+
+cf.read_my_weird_data(...)
---
+++ Demonstrations/example_data_analysis_project/common_functions.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def read_my_weird_data(path):
+    # some code which reads in the data from path
+
+    return ...
